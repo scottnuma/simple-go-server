9fb3b65d599b6f29e4f038084583c94778fe3486
scottnuma <scott.numamoto@gmail.com>
2021-06-23 17:22:35 -0700

    Basic

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/scottnuma/simple-go-server
+
+go 1.16
---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+func main() {
+	s := &http.Server{
+		Addr: os.Getenv("ADDRESS"),
+		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			fmt.Fprintf(w, "hello world")
+		}),
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+	log.Fatal(s.ListenAndServe())
+}
